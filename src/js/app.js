3db604f242bbd1c397d5b0af78c433fedb31c3eb
Khrapov Denys <khrapov.denys@gmail.com>
2023-06-02 13:04:13 +0300

    added cta section and styles

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function ($) {
 	testWebPFunction();
 	initAccordion();
+	initSelectDropDown();
 });
 
 
@@ -37,4 +38,20 @@ function initAccordion() {
 	  });
 	}
 }
+
+
+function initSelectDropDown(){
+	$('.select-dropdown__button').on('click', function(e){
+		e.preventDefault();
+		$(this).toggleClass('active');
+		$('.select-dropdown__list').toggleClass('active');
+	});
+	$('.select-dropdown__list-item').on('click', function(){
+		var itemValue = $(this).data('value');
+		$('.select-dropdown__button span').text($(this).text()).parent().attr('data-value', itemValue);
+		$('.select-dropdown__list').toggleClass('active');
+		$('.select-dropdown__button').toggleClass('active');
+	});
+}
+
 
