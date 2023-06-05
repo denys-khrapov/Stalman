6b128049302685470ba8e80131003db60ffa643b
Khrapov Denys <khrapov.denys@gmail.com>
2023-06-05 21:42:54 +0300

    added header section and styles

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,6 +5,8 @@ jQuery(document).ready(function ($) {
 	initSlick();
 	initSwiper();
 	initHoverandClickCard();
+	initMobileMenu();
+	initScrollTo();
 });
 
 
@@ -241,4 +243,57 @@ function initHoverandClickCard(){
 
 
 
+function initMobileMenu() {
+	const headerBurger = $('.header__burger');
+	const headerMenu = $('.header__menu');
+	const body = $('body');
+	const headerMenuLink = $('.header__link');
+	const headerMenuBtnEnroll = $('.btn-enroll');
+	const headerOverlay = $('.overlay');
 
+
+	headerBurger.on('click', function () {
+		headerBurger.toggleClass('active');
+		headerMenu.toggleClass('active');
+		body.toggleClass('lock');
+	});
+
+	headerOverlay.on('click', function () {
+		if (body.hasClass('lock') && headerMenu.hasClass('active') && headerBurger.hasClass('active')) {
+			body.removeClass('lock');
+			headerMenu.removeClass('active');
+			headerBurger.removeClass('active');
+		}
+	});
+
+	headerMenuLink.on('click', function () {
+		if (body.hasClass('lock') && headerMenu.hasClass('active') && headerBurger.hasClass('active')) {
+			body.removeClass('lock');
+			headerMenu.removeClass('active');
+			headerBurger.removeClass('active');
+		}
+	});
+
+	headerMenuBtnEnroll.on('click', function () {
+		console.log('test');
+		if (body.hasClass('lock') && headerMenu.hasClass('active') && headerBurger.hasClass('active')) {
+			body.removeClass('lock');
+			headerMenu.removeClass('active');
+			headerBurger.removeClass('active');
+		}
+	});
+
+}
+
+
+function initScrollTo() {
+	$("a.scroll-to").click(function () {
+		$("html, body").animate({
+			scrollTop: $($(this).attr("href")).offset().top + "px"
+		}, {
+			duration: 700,
+			easing: "swing"
+		});
+		return false;
+	});
+}
